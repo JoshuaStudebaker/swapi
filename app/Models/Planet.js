b348d06fbc408e1b8143dd2e3ae0025188ae4332
JoshuaStudebaker <j.s.studebaker1@gmail.com>
2020-08-31 15:10:37 -0600

    added planet model

---
+++ app/Models/Planet.js
@@ -0,0 +1,13 @@
+export default class Planet {
+  constructor({ name, climate, terrain, population }) {
+    this.name = name;
+    this.climate = climate;
+    this.terrain = terrain;
+    this.population = population;
+  }
+  get Template() {
+    return `
+    
+    `;
+  }
+}
